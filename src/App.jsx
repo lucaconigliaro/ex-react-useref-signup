dd5e9dbbd9e111a093327e39e437e3b73c2bdd61
lucaconigliaro <lucaconigliaro1@gmail.com>
2025-03-29 19:44:43 +0100

    Milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
+
+const letters = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()-_=+[]{}|;:'\,.<>?/`~";
 
 function App() {
 
@@ -11,16 +15,24 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!name || !username || !password || !specialization || !experience || !description) {
+    if (!name.trim() || !username.trim() || !password.trim() || !specialization.trim() || !experience.trim() || !description.trim()) {
       alert("Compila tutti i campi");
       return;
-    }
-    if (experience <= 0) {
+    } else if (experience <= 0) {
       alert("Gli anni di esperienza devono essere un numero positivo");
       return;
-    }
-    if (specialization === "") {
+    } else if (specialization === "") {
       alert("Seleziona una specializzazione");
+      return;
+    } else if (!isUsernameValid) {
+      alert("Username non valido");
+      return;
+    } else if (!isPasswordValid) {
+      alert("Password non valida");
+      return;
+    } else if (!isDescriptionValid) {
+      alert("Descrizione non valida");
+      return;
     }
     console.log({
       name,
@@ -31,6 +43,28 @@ function App() {
       description
     });
   };
+
+  const isUsernameValid = useMemo(() => {
+    const charsValid = username.split("").every(char =>
+      letters.includes(char.toLowerCase()) ||
+      numbers.includes(char)
+    );
+    return charsValid && username.trim().length >= 6;
+  }, [username]);
+
+  const isPasswordValid = useMemo(() => {
+    return (
+      password.trim().length >= 8 &&
+      password.split("").some(char => letters.includes(char)) &&
+      password.split("").some(char => numbers.includes(char)) &&
+      password.split("").some(char => symbols.includes(char))
+    )
+  }, [password]);
+
+  const isDescriptionValid = useMemo(() => {
+    return description.trim().length >= 100 &&
+      description.trim().length <= 1000;
+  }, [description])
 
   return (
     <div className="container">
@@ -54,6 +88,11 @@ function App() {
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
+          {username.trim() && (
+            <p style={{ color: isUsernameValid ? `green` : `red` }}>
+              {isUsernameValid ? "Username valido" : "Deve contenere almeno 6 caratteri alfanumerici"}
+            </p>
+          )}
         </div>
 
         <div>
@@ -64,6 +103,11 @@ function App() {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
+          {password.trim() && (
+            <p style={{ color: isPasswordValid ? `green` : `red` }}>
+              {isPasswordValid ? "Password valida" : "Deve contenere almeno 8 caratteri, 1 letta, 1 numero, 1 simbolo"}
+            </p>
+          )}
         </div>
 
         <div>
@@ -97,6 +141,11 @@ function App() {
             value={description}
             onChange={(e) => setDescription(e.target.value)}
           />
+          {description.trim() && (
+            <p style={{ color: isDescriptionValid ? `green` : `red` }}>
+              {isDescriptionValid ? "Descrizione valida" : "Deve contenere almeno tra 100 e 1000 caratteri"}
+            </p>
+          )}
         </div>
 
         <button type="submit">Registrati</button>
